2ace2f09d83ddf1d0e6ff271a0e28f2e173a59cb
Brian <briantang31@gmail.com>
2024-09-12 12:11:18 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      "./**/*.{html,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          inter: ["Inter", "sans-serif"],
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{html,js,ts,jsx,tsx}",
+    "./styles.css", // Include your new CSS file
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
