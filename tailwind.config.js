935c9c0e22dc93efef2f50ae6da1f069ba5972de
Brian <briantang31@gmail.com>
2024-09-12 12:39:43 -0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./**/*.{html,js,ts,jsx,tsx}",
-    "./styles.css", // Include your new CSS file
+    "./style.css", // Include your new CSS file
   ],
   theme: {
     extend: {
