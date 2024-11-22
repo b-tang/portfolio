1c29a9ea736c47530dcc17ba437957181d170256
Brian <briantang31@gmail.com>
2024-11-22 13:48:22 -0800

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(e.target.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
