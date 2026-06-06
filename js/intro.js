15fbaa89868f3e2b85e2fb330ebd8efb35cf9858
Brian <briantang31@gmail.com>
2026-06-06 10:20:35 -0700

    formatted code to prettier

--- js/intro.js
+++ js/intro.js
@@ -1,4 +1,4 @@
-let header = document.querySelector('#intro');
+let header = document.querySelector("#intro");
 // let anim = [
 //     { t: "{ }", ms: 200 },
 //     { t: "{_}", ms: 200 },
@@ -22,46 +22,45 @@ let header = document.querySelector('#intro');
 //     { t: "{Brian Tang}", ms: 200 }
 // ];
 let anim = [
-    { t: "> _", ms: 200 },
-    { t: ">__", ms: 200 },
-    { t: "> _", ms: 200 },
-    { t: ">__", ms: 200 },
-    { t: ">B__", ms: 100 },
-    { t: ">BR__", ms: 100 },
-    { t: ">BRI__", ms: 100 },
-    { t: ">BRIA__", ms: 100 },
-    { t: ">BRIAN__", ms: 100 },
-    { t: ">BRIAN___", ms: 100 },
-    { t: ">BRIAN_T__", ms: 100 },
-    { t: ">BRIAN_TA__", ms: 100 },
-    { t: ">BRIAN_TAN__", ms: 100 },
-    { t: ">BRIAN_TANG__", ms: 100 },
-    { t: "{BRIAN_TANG }", ms: 200 },
-    { t: "{BRIAN_TANG_}", ms: 200 },
-    { t: "{BRIAN_TANG }", ms: 200 },
-    { t: "{BRIAN_TANG_}", ms: 200 },
-    { t: "{BRIAN_TANG}", ms: 200 },
-    { t: "{BRIAN_TANG}", ms: 200 }
+  { t: "> _", ms: 200 },
+  { t: ">__", ms: 200 },
+  { t: "> _", ms: 200 },
+  { t: ">__", ms: 200 },
+  { t: ">B__", ms: 100 },
+  { t: ">BR__", ms: 100 },
+  { t: ">BRI__", ms: 100 },
+  { t: ">BRIA__", ms: 100 },
+  { t: ">BRIAN__", ms: 100 },
+  { t: ">BRIAN___", ms: 100 },
+  { t: ">BRIAN_T__", ms: 100 },
+  { t: ">BRIAN_TA__", ms: 100 },
+  { t: ">BRIAN_TAN__", ms: 100 },
+  { t: ">BRIAN_TANG__", ms: 100 },
+  { t: "{BRIAN_TANG }", ms: 200 },
+  { t: "{BRIAN_TANG_}", ms: 200 },
+  { t: "{BRIAN_TANG }", ms: 200 },
+  { t: "{BRIAN_TANG_}", ms: 200 },
+  { t: "{BRIAN_TANG}", ms: 200 },
+  { t: "{BRIAN_TANG}", ms: 200 },
 ];
 let stepDenominator = 1;
 if (window.localStorage.stepDenominator)
-    stepDenominator = window.localStorage.stepDenominator;
+  stepDenominator = window.localStorage.stepDenominator;
 let i = 0;
 let update = () => {
-    let step = anim[i];
-    header.innerText = step.t;
-    i++;
+  let step = anim[i];
+  header.innerText = step.t;
+  i++;
 
-    if (i < anim.length)
-        setTimeout(update, step.ms / stepDenominator);
-    else {
-        header.classList.add('top');
-        setTimeout(() => {
-            const main = document.getElementById('main');
-            main.style.opacity = 1;
-            main.style.pointerEvents = 'auto';
-        }, 500);
-        window.localStorage.stepDenominator = 2;
-    }
-}
+  if (i < anim.length) setTimeout(update, step.ms / stepDenominator);
+  else {
+    header.classList.add("top");
+    setTimeout(() => {
+      const main = document.getElementById("main");
+      main.style.opacity = 1;
+      main.style.pointerEvents = "auto";
+    }, 500);
+    window.localStorage.stepDenominator = 2;
+  }
+};
 update();
